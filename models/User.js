dde113b29ca9ab661e26fe9944298ded683344c3
Prem <premsai200804@gmail.com>
2025-06-15 03:02:07 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -12,7 +12,8 @@ const userSchema = new mongoose.Schema({
     required: true,
     unique: true,
     trim: true,
-    lowercase: true
+    lowercase: true,
+    match: [/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, 'Please enter a valid email address']
   },
   password: {
     type: String,
